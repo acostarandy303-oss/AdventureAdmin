e026d91dff9b7241ab9d872f2668847402448c76
acostarandy303-oss <acostarandy303@gmail.com>
2026-03-17 10:52:56 -0800

    Agregar Business_Entity

---
+++ AdventureAdmin.Ui/Business_Entity/Business_EntityForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AdventureAdmin.Ui.Business_Entity
+{
+    public partial class Business_EntityForm : Form
+    {
+        public Business_EntityForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
